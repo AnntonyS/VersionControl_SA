1a2477728c2385c7dd499c6fe10e36c740731fe0
Antal Schumacher <antal.schumacher@stud.uni-corvinus.hu>
2021-11-27 17:28:08 +0100

    First steps

---
+++ UserMaintenance_SA/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance_SA.Entities
+{
+    class User
+    {
+        public Guid guid { get; set; } = Guid.NewGuid();
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //private string _fullname;
+
+        public string Fullname 
+        {
+            get { return LastName + " " + FirstName; }
+        }
+    }
+}
