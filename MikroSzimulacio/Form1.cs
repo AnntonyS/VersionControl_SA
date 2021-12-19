5474963a551b08dacd8798872cbdf28330768e00
Antal Schumacher <antal.schumacher@stud.uni-corvinus.hu>
2021-12-19 19:56:55 +0100

    Random generator and simulation

--- MikroSzimulacio/Form1.cs
+++ MikroSzimulacio/Form1.cs
@@ -18,6 +18,8 @@ namespace MikroSzimulacio
         List<BirthProbabilities> BirthProbabilities = new List<BirthProbabilities>();
         List<DeathProbabilities> DeathProbabilities = new List<DeathProbabilities>();
 
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,49 @@ namespace MikroSzimulacio
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
             MessageBox.Show(DeathProbabilities.Count.ToString());
+
+            for (int year = 2005 ; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SzimulaciosLepes(Population[i], year);
+                }
+
+                int ferfiakszama = (from x in Population where x.Gender == Gender.Male select x).Count();
+                int nokszama = (from x in Population where x.Gender == Gender.Female select x).Count();
+
+                Console.WriteLine(string.Format("Év: {0} Férfiak: {1} Nők: {2} ", year, ferfiakszama, nokszama));
+            }
+            
+        }
+
+        private void SzimulaciosLepes(Person person, int year)
+        {
+            if (!person.IsAlive) return;
+            int kor = year - person.Birthyear;
+            //halálozás valószínűsége
+            double halalvaloszinuseg = (from x in DeathProbabilities where x.Gender == person.Gender && x.Age == kor select x.P).FirstOrDefault();
+            double veletlen = rng.NextDouble();
+            if (veletlen <= halalvaloszinuseg)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double szuletesvaloszinuseg = (from x in BirthProbabilities where x.Age == kor select x.P).FirstOrDefault();
+                veletlen = rng.NextDouble();
+                if (veletlen <= szuletesvaloszinuseg)
+                {
+                    Person baba = new Person();
+                    baba.Birthyear = year;
+                    baba.NbrOfChildren = 0;
+                    baba.Gender = (Gender)rng.Next(1, 3);
+                    Population.Add(baba);
+                }    
+                    
+            }
+
+
+
         }
 
         public List<Person> GetPopulation(string csvpath)
